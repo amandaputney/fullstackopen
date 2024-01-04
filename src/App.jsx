258964625f013b1e3a3bf00be90f0d4266420a98
asp <amandasueputney@gmail.com>
2024-01-04 13:27:18 -0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Lee'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <h4><Hello name='Amanda'age={26 + 10}/></h4>
+      <h2><Hello name='Goldie' /></h2>
+      <Hello name={name} age={age}/>
+    </div>
+  )
+}
+
+export default App
