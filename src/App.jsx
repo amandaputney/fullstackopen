8c6ec1b047177149b6eadcbd4430d226c87e7076
asp <amandasueputney@gmail.com>
2024-01-19 13:16:05 -0800

    completes part 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,25 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://amandaputney.github.io/portfolio/' target="blank">Amanda</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = 'Lee'
   const age = 10
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
 
       <h4><Hello name='Amanda'age={26 + 10}/></h4>
       <Hello name={name} age={age}/>
-    </div>
+      <Footer />
+    </>
   )
 }
 
